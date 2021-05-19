599c7a6545915904aae1e782c43ef897b4c18974
Ahmed Bule <ahmedbule01@gmail.com>
2021-05-19 15:21:55 +0300

    completed  the functionality of the play and pause

--- script.js
+++ script.js
@@ -6,9 +6,52 @@ const timestamp = document.getElementById('timestamp');
 
 
 
+// Play and pause video
+
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+//Update play/stop icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = `<i class="fa fa-play fa-2x"></i>`
+    } else {
+        play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`
+
+    }
+}
+
+//Update progress/timestamp
+
+function updateProgress() {
+    return true
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    return true
+}
+
+// Stop function 
+function stopVideo() {
+    return true
+}
+
 // We will have a bunch of event listeners 
 
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timestamp', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
+
